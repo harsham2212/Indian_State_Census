b0064334d5886046febc13d528f1aab03b49e9fb
Harsh Singh <kmrharsh50@gmail.com>
2022-01-06 16:40:49 +0530

    [Harsh]Added:Welcome Meassage Indian_State_Census_Analyser_Problem feature implemented

---
+++ Indian_State_Census/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indian_State_Census
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser Problem");
+        }
+    }
+}
